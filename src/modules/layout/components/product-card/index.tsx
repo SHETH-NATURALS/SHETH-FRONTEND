f8be31c2cb5839667c1c58fc283ed5d68e0fbd8c
WalobwaD <walobwadan@gmail.com>
2024-05-13 15:57:34 +0300

    linked products

--- src/modules/layout/components/product-card/index.tsx
+++ src/modules/layout/components/product-card/index.tsx
@@ -4,44 +4,54 @@ import { retrievePricedProductById } from "@lib/data"
 import { getProductPrice } from "@lib/util/get-product-price"
 import PreviewPrice from "@modules/products/components/product-preview/price"
 import Image from "next/image"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
 
 interface ProductProps {
-    productPreview: ProductPreviewType,
-    isFeatured?:boolean,
-    region: Region
+  productPreview: ProductPreviewType
+  isFeatured?: boolean
+  region: Region
 }
 
-const ProductCard = async ({ productPreview, isFeatured, region}: ProductProps) => {
-    const pricedProduct = await retrievePricedProductById({
-        id: productPreview.id,
-        regionId: region.id,
-      }).then((product) => product)
-    
-      if (!pricedProduct) {
-        return null
-      }
-    
-      const { cheapestPrice } = getProductPrice({
-        product: pricedProduct,
-        region,
-      })
-    return (
-        <div className="h-[500px] w-auto bg-white flex flex-col flex-shrink-0 relative">
-            <div className="h-[384px]">
-                <Image
-                    src={productPreview.thumbnail ?? ''}
-                    alt={productPreview.title}
-                    width={384}
-                    height={384}
-                />
-            </div>
-            <div className="flex flex-col text-center justify-end absolute w-full h-full p-3 gap-4">
-                <p className="font-semibold text-primary">{ productPreview.title }</p>
-                <p>Product Description</p>
-                { cheapestPrice && <PreviewPrice price={cheapestPrice}/>}
-            </div>
+const ProductCard = async ({
+  productPreview,
+  isFeatured,
+  region,
+}: ProductProps) => {
+  const pricedProduct = await retrievePricedProductById({
+    id: productPreview.id,
+    regionId: region.id,
+  }).then((product) => product)
+
+  if (!pricedProduct) {
+    return null
+  }
+
+  const { cheapestPrice } = getProductPrice({
+    product: pricedProduct,
+    region,
+  })
+  return (
+    <>
+      <LocalizedClientLink
+        href={`/products/${productPreview.handle}`}
+        className="h-[500px] w-auto bg-white flex flex-col flex-shrink-0 relative"
+      >
+        <div className="h-[384px]">
+          <Image
+            src={productPreview.thumbnail ?? ""}
+            alt={productPreview.title}
+            width={384}
+            height={384}
+          />
         </div>
-    )
+        <div className="flex flex-col text-center justify-end absolute w-full h-full p-3 gap-4">
+          <p className="font-semibold text-primary">{productPreview.title}</p>
+          <p>Product Description</p>
+          {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
+        </div>
+      </LocalizedClientLink>
+    </>
+  )
 }
 
 export default ProductCard
